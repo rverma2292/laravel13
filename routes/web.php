3016a1751c484fb7eab619efef0d80845932f27c
rahul-laravel13 <rverma2292@gmail.com>
2026-04-30 18:21:38 +0530

    called controller method

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +10,5 @@ Route::get('/', function () {
 Route::get('/greeting', function () {
    return "Hello World";
 });
+
+Route::get('/user', [UserController::class, 'index']);
