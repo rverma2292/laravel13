41efaa85d17d21a341c5c01a5e74dd4d2f24566c
rahul-laravel13 <rverma2292@gmail.com>
2026-04-30 18:44:23 +0530

    installed api route( auto add sanctum )

--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,8 @@ Route::get('/greeting', function () {
    return "Hello World";
 });
 
-Route::get('/user', [UserController::class, 'index']);
+
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
