bdae6d81c318c40067e1829e37916115b9dfb3f8
rahul-laravel13 <rverma2292@gmail.com>
2026-05-02 15:56:03 +0530

    Routing examples

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Models\User;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,12 +12,6 @@ Route::get('/', function () {
 Route::get('/greeting', function () {
    return "Hello World";
 });
-
-
-
-//Route::get('/user', function (Request $request) {
-//    return $request->user();
-//})->middleware('auth:sanctum');
 
 Route::middleware([
     'auth:sanctum',
@@ -26,3 +22,49 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+Route::match(['get', 'post'], '/test-match', function (Request $request) {
+    return "You accessed this page using: " . $request->method();
+});
+
+Route::any('/test-any', function (Request $request) {
+    return "You accessed this page using: " . $request->method();
+});
+
+Route::get('/posts/{post}/comments/{comment}', function (string $postId, string $commentId) {
+    return "post: ".$postId . ' comment: ' . $commentId;
+});
+
+Route::get('employee/{id}', function (string $name) {
+    return $name;
+});
+
+//optional route
+//Route::get('employee/{name?}', function (string $name = null) {
+//    return $name ?? 'Employee';
+//})->where('name', '[A-Za-z]+');
+
+Route::get('/search/{search}', function (string $search) {
+    return $search;
+})->where('search', '.*');
+
+Route::domain('{tenant}.laravel13')->group(function () {
+    Route::get('test/{id}', function (string $tenant, string $id) {
+        return "Tenant: " . $tenant . ' User ID: ' . $id;
+    });
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/amaze', function () {
+        return "Admin Dashboard URL";
+    });
+});
+
+Route::get('user/{user}', function (User $user){
+    return $user->email;
+})->name('userprofile');
+
+//redirect example
+Route::redirect('/here', '/greeting');
+Route::redirect('/there', '/greeting', 301);
+Route::permanentRedirect('permanent', '/greeting');
