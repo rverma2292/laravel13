e3397fc2d04969ce4e5c76d5947647ad646f6bc5
rahul-laravel13 <rverma2292@gmail.com>
2026-05-09 16:00:33 +0530

    artisan console comman example

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::Command('test', function () {
+    $this->info('This is a test command. It does not perform any actions but serves as an example of how to create a custom Artisan command in Laravel.');
+})->purpose('Display an test command text.');
